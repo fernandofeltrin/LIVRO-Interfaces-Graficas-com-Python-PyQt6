07b68388c042d0171466bfbfb89778cc63da1966
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-07-14 21:43:02 -0300

    Update inserindo botoes basicos.py

--- inserindo botoes basicos.py
+++ inserindo botoes basicos.py
@@ -1,58 +1,27 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QIcon
 
 class JanelaPrincipal(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Meu Programa') # Define manualmente o título da janela.
-        self.setGeometry(150, 150, 500, 500) # Por justaposição, o primeiro parâmetro define a distância em pixels da lateral esquerda da tela até a janela, seguido do segundo parâmetro que define a distância em pixels do topo da tela até a borda superior da janela. Na sequência os dois últimos parâmetros se referem a largura e a altura da janela, respectivamente.
+        self.setWindowIcon(QIcon('icone.png'))
+        self.setGeometry(150, 150, 480, 320) # Por justaposição, o primeiro parâmetro define a distância em pixels da lateral esquerda da tela até a janela, seguido do segundo parâmetro que define a distância em pixels do topo da tela até a borda superior da janela. Na sequência os dois últimos parâmetros se referem a largura e a altura da janela, respectivamente.
         self.Interface() # Instancia o método de classe Interface()
 
     def Interface(self):
-        layout = QFormLayout()
+        texto1 = QLabel('Olá Mundo!!!', self) # Será criada uma linha de texto na posição inicial da janela, que por padrão é o canto superior esquerdo
+        texto1.move(100, 50) # Define manualmente a posição onde texto1 será inserida, nesse caso, 100 pixels a partir da esquerda, 50 pixels a partir do topo. Se não definir este parâmetro para os labels, seus respectivos textos aparecerão sobrepostos. O Método move( ) serve para qualquer elemento visual da janela.
 
-        texto_usuario = QLabel('Usuário: ')
-        input_usuario = QLineEdit()
-        input_usuario.setPlaceholderText('Digite seu nome de usuário')
-        texto_senha = QLabel('Senha: ')
-        input_senha = QLineEdit()
-        input_senha.setPlaceholderText('Digite sua senha')
+        botao1 = QPushButton('OK', self) # Será inserido um botão na posição inicial da janela (canto superior esquerdo) que neste momento ao clicar não realiza nenhuma ação, pois o botão inicial é apenas um botão, sem nada pré-configurado para o mesmo.
+        botao1.move(100, 200)
 
-        layout.addRow(texto_usuario, input_usuario) # Insere uma linha contendo o campo 'Usuário: ' seguido do campo para inserção de texto
-        layout.addRow(texto_senha, input_senha) # Insere uma segunda linha, agora contendo os campos 'Senha: ' e o campo para inserção da senha
-        layout.addRow(QLabel('Salvar Informações: '), QComboBox()) # Ao criar a linha, já insere na mesma um texto/label e um campo para seleção
+        texto2 = QLabel('Fernando Feltrin', self)
+        texto2.move(200, 50)
 
-        layout_secundario = QHBoxLayout() # Contornando o problema, podemos criar uma estrutura de layout secundária, de outro formato, para que possamos reorganizar os elementos
-        layout_secundario.addStretch()
-        layout_secundario.addWidget(QPushButton('ENTRAR')) # Dentro desse layout secundário criamos os botões que queremos
-        layout_secundario.addWidget(QPushButton('SAIR'))
-        layout_secundario.addStretch()
-        layout.addRow(layout_secundario) # Instanciamos novamente o layout geral de nossa janela, incorporando na mesma, dentro de uma nova linha, o layout secundário e seus elementos.
-
-        layout_terciario = QVBoxLayout()
-        layout_terciario.addWidget(QLabel('Quando de memória deseja alocar?'))
-        self.slider = QSlider(Qt.Horizontal) # Cria um slider genérico, caso não seja repassado nenhum parâmetro, será um slider vertical
-        self.slider.setMinimum(20)
-        self.slider.setMaximum(100)
-        self.slider.setTickPosition(QSlider.TicksAbove)
-        self.slider.valueChanged.connect(self.recebe_valor) # No caso de um slider, o método que conecta o gatilho para executar uma ação em uma função é valueChanged.connect( )
-        self.valor = QLabel('0')
-        self.valor.setAlignment(Qt.AlignCenter)
-        layout_terciario.addWidget(self.valor)
-        layout_terciario.addWidget(self.slider)
-
-        layout.addRow(layout_terciario)
-
-        self.setLayout(layout)
         self.show()
-
-    def recebe_valor(self):
-        val_escolhido = self.slider.value()
-        self.valor.setText(str(val_escolhido))
-        print(f'Valor escolhido: {val_escolhido}%')
 
 qt = QApplication(sys.argv)
 app = JanelaPrincipal()
-sys.exit(qt.exec_())
+sys.exit(qt.exec())
