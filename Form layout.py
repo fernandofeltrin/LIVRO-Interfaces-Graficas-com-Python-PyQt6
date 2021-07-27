d32acc9488d852f00dd0bd04c1adae7eebba9384
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-07-27 15:57:28 -0300

    Update Form layout.py

--- Form layout.py
+++ Form layout.py
@@ -20,20 +20,20 @@ class JanelaPrincipal(QWidget):
         input_senha = QLineEdit()
         input_senha.setPlaceholderText('Digite sua senha')
         input_senha.setEchoMode(QLineEdit.EchoMode.Password)
-        seleciona_ambiente = QComboBox()  # Cria uma caixa de seleção inicialmente vazia.
-        seleciona_ambiente.addItem('Ambiente Comum')  # Primeiro elemento da caixa de seleção
-        seleciona_ambiente.addItem('Painel de Controle')  # Segundo elemento da caixa de seleção
+        seleciona_ambiente = QComboBox()
+        seleciona_ambiente.addItem('Ambiente Comum')
+        seleciona_ambiente.addItem('Painel de Controle')
 
-        layout.addRow(texto_usuario, input_usuario) # Insere uma linha contendo o campo 'Usuário: ' seguido do campo para inserção de texto
-        layout.addRow(texto_senha, input_senha) # Insere uma segunda linha, agora contendo os campos 'Senha: ' e o campo para inserção da senha
-        layout.addRow(QLabel('Salvar Informações: '), seleciona_ambiente) # Ao criar a linha, já insere na mesma um texto/label e um campo para seleção
+        layout.addRow(texto_usuario, input_usuario) 
+        layout.addRow(texto_senha, input_senha) 
+        layout.addRow(QLabel('Salvar Informações: '), seleciona_ambiente)
 
-        layout_secundario = QHBoxLayout() # Contornando o problema, podemos criar uma estrutura de layout secundária, de outro formato, para que possamos reorganizar os elementos
-        layout_secundario.addStretch() # Insere um espaço antes dos botões
-        layout_secundario.addWidget(QPushButton('ENTRAR')) # Dentro desse layout secundário criamos os botões que queremos
+        layout_secundario = QHBoxLayout()
+        layout_secundario.addStretch()
+        layout_secundario.addWidget(QPushButton('ENTRAR'))
         layout_secundario.addWidget(QPushButton('SAIR'))
-        layout_secundario.addStretch() # Insere um espaço depois dos botões, assim, centralizando os mesmos dentro do layout
-        layout.addRow(layout_secundario) # Instanciamos novamente o layout geral de nossa janela, incorporando na mesma, dentro de uma nova linha, o layout secundário e seus elementos.
+        layout_secundario.addStretch()
+        layout.addRow(layout_secundario)
 
         self.setLayout(layout)
         self.show()
